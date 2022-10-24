8ee333fe1d021b39e83ab4218c69130f466cb1d9
ihmohamed12 <55826575+ihmohamed12@users.noreply.github.com>
2022-10-24 16:17:44 +0200

    Delete lib directory

--- lib/test.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter/material.dart';
-// class Testing extends StatelessWidget {
-//   const ({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
